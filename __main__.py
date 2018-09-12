1a215105f032cdf477cacc55e009237ae397c57d
scott1928 <scott1928@gmail.com>
2018-09-12 21:26:08 +0100

    deleted old code

--- __main__.py
+++ __main__.py
@@ -18,49 +18,8 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 
-
-def tweet_downloader():
-    search_query = "TLRY"
-    max_tweets = 10000
-    tweets_per_qry = 100
-    fname = 'tweets.txt'
-    sinceID = None
-    max_id = -1
-    tweetCount = 0
-    print("Downloading max {0} tweets".format(max_tweets))
-    with open(fname,'w') as f:
-        while tweetCount < max_tweets:
-            try:
-                if (max_id <= 0):
-                    if (not sinceID):
-                        new_tweets = api.search(q=search_query, count=tweets_per_qry)
-                    else:
-                        new_tweets = api.search(q=search_query,count=tweets_per_qry,sinceID=sinceID)
-                else:
-                    if (not sinceID):
-                        new_tweets = api.search(q=search_query, count=tweets_per_qry,max_id=str(max_id -1))
-                    else:
-                        new_tweets = api.search(q=search_query, count=tweets_per_qry,max_id=str(max_id -1),sinceID=sinceID)
-                if not new_tweets:
-                    print("no new tweets found")
-                    break
-                for tweet in new_tweets:
-                    f.write(tweet.full_text)
-#                    f.write(jsonpickle.encode(tweet._json, unpicklable=False) +
-#                            '\n')
-                tweetCount += len(new_tweets)
-                print("Downloaded {0} tweets".format(tweetCount))
-                max_id = new_tweets[-1].id
-            except tweepy.TweepError as e:
-                # Just exit if any error
-                print("some error : " + str(e))
-                break
-    print ("Downloaded {0} tweets, Saved to {1}".format(tweetCount, fName))
-
-#tweet_downloader()
-
 def main_function():
-    search = ["TLRY","PANW","APH","ATRS","TNDM"]
+    search = ["TLRY","PANW","APH","ATRS","TNDM","$VOD"]
     numberOfTweets = 100
     scores_list = []
     tweet_count = 0
